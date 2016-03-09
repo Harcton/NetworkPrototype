315a2cfe650289d994c5af8096eca34f18c910ed
Teo Hiltunen <tejo94@luukku.com>
2016-03-09 16:14:36 +0200

    External IP connection works
    But for only once per server run...

--- SpehsEngineNetworkPrototype/Network.h
+++ SpehsEngineNetworkPrototype/Network.h
@@ -1,4 +1,4 @@
 #pragma once
-#define PORT_NUMBER 25565
+#define PORT_NUMBER 41624
 //41624
 //13
--- SpehsEngineNetworkPrototype/UDPClient.cpp
+++ SpehsEngineNetworkPrototype/UDPClient.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <SpehsEngine/Console.h>
 #include "UDPClient.h"
+#include "Network.h"
 
 UDPClient::UDPClient(){}
 UDPClient::~UDPClient(){}
@@ -13,28 +14,33 @@ void UDPClient::run(std::string str)
 		spehs::console::log("Creating UDP client...");
 		boost::asio::io_service ioService;
 		boost::asio::ip::udp::resolver resolver(ioService);
-		boost::asio::ip::udp::resolver::query query(boost::asio::ip::udp::v4(), str, "daytime");
+		boost::asio::ip::udp::resolver::query query(boost::asio::ip::udp::v4(), str, std::to_string(PORT_NUMBER));
 		boost::asio::ip::udp::endpoint receiverEndpoint = *resolver.resolve(query);
 
 		boost::asio::ip::udp::socket socket(ioService);
 		socket.open(boost::asio::ip::udp::v4());
-		boost::array<char, 1> sendBuffer = { { 0 } };//Sends 0 to server (?)
-		socket.send_to(boost::asio::buffer(sendBuffer), receiverEndpoint);
 
-		boost::array<char, 1024> receiveBuffer;
-		boost::asio::ip::udp::endpoint senderEndpoint;
-		spehs::console::log("UDP client: waiting for server response...");
-		size_t length = socket.receive_from(boost::asio::buffer(receiveBuffer), senderEndpoint);
+			boost::array<char, 1> sendBuffer = { { 0 } };//Sends 0 to server (?)
+			boost::array<char, 1024> receiveBuffer;
+			boost::asio::ip::udp::endpoint senderEndpoint;
 
-		//Output received data
-		std::cout << "\nUDPClient::receive buffer :";
-		std::cout.write(receiveBuffer.data(), length);
+		for (unsigned i = 0; i < 2; i++)
+		{
 
-		std::string receiveBufferAsString;
-		receiveBufferAsString.resize(length);
-		for (unsigned i = 0; i < length; i++)
-			receiveBufferAsString[i] = receiveBuffer[i];
-		spehs::console::log("UDPClient::receive buffer: " + receiveBufferAsString);
+			spehs::console::log("UDP client: waiting for server response...");
+			size_t length = socket.receive_from(boost::asio::buffer(receiveBuffer), senderEndpoint);
+			socket.send_to(boost::asio::buffer(sendBuffer), receiverEndpoint);//Server closes existing connection
+
+			//Output received data
+			std::cout << "\nUDPClient::receive buffer :";
+			std::cout.write(receiveBuffer.data(), length);
+
+			std::string receiveBufferAsString;
+			receiveBufferAsString.resize(length);
+			for (unsigned i = 0; i < length; i++)
+				receiveBufferAsString[i] = receiveBuffer[i];
+			spehs::console::log("UDPClient::receive buffer: " + receiveBufferAsString);
+		}
 
 	}
 	catch (std::exception& e)
